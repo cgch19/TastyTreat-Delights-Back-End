d4c5424b9ead23335e3d3e1719c1a1435d68b7af
cgch8 <carloschie15@gmail.com>
2024-05-20 15:53:56 -0700

    fixed the working fetch user and the mongodb

--- controllers/treatsController.js
+++ controllers/treatsController.js
@@ -1,16 +1,17 @@
 const db = require ('../models/Treats')
+const Treat = require('../models/YourTreats')
+
 
 // Function to get your treats
 const getYourtreats = async (req, res) => {
   try {
     console.log('getYourtreats called');
-    console.log('Request user ID:', req.user ? req.user.id : 'No user ID');
 
-    const foundTreats = await db.Treat.find({ User: req.user.id });
+    const foundTreats = await Treat.find({});
     console.log('Found treats:', foundTreats);
 
     if (!foundTreats || foundTreats.length === 0) {
-      console.log('No treats found for user:', req.user.id);
+      console.log('No treats found');
       res.status(404).json({ message: 'Cannot find treats' });
     } else {
       res.status(200).json({ data: foundTreats });
@@ -20,24 +21,17 @@ const getYourtreats = async (req, res) => {
     res.status(400).json({ error: err.message });
   }
 };
-
 // Function to create your treats
 const createYourtreats = async (req, res) => {
   try {
     console.log('createYourtreats called');
     console.log('Request body:', req.body);
-    console.log('Request user ID:', req.user ? req.user.id : 'No user ID');
 
-    const createdYourTreats = await db.Treat.create(req.body );
+    const createdYourTreats = new Treat(req.body);
     await createdYourTreats.save();
     console.log('Saved created treats:', createdYourTreats);
 
-    if (!createdYourTreats) {
-      console.log('Failed to create treats for user:', req.user.id);
-      res.status(400).json({ message: 'Cannot create your treats' });
-    } else {
-      res.status(201).json({ data: createdYourTreats, message: 'Your treats created' });
-    }
+    res.status(201).json({ data: createdYourTreats, message: 'Your treats created' });
   } catch (err) {
     console.error('Error creating treats:', err.message);
     res.status(400).json({ error: err.message });
