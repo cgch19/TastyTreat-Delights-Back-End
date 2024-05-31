48921cfe6fa6574d771a726f15252f773c551cc7
cgch8 <carloschie15@gmail.com>
2024-05-31 16:23:38 -0700

    Improve MongoDB connection handling

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const treatsRouter = require('./controllers/index'); 
-
+const treatsRouter = require('./controllers/index');
 
 const app = express();
 const { PORT, MONGODB_URI } = process.env;
@@ -13,17 +12,14 @@ const { PORT, MONGODB_URI } = process.env;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// MongoDB connection
-mongoose.connect(MONGODB_URI, {});
-const db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
-  console.log('Connected to MongoDB');
-});
+// MongoDB connection
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Error connecting to MongoDB:', err));
 
 // Use the routes
-app.use('/api', treatsRouter); 
+app.use('/api', treatsRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
