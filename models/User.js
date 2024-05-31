a25430a665f2e346e59d921c1c38b4535c917790
cgch8 <carloschie15@gmail.com>
2024-05-30 20:19:38 -0700

    added some sources

--- models/User.js
+++ models/User.js
@@ -5,8 +5,10 @@ const userSchema = new mongoose.Schema({
     username: {type: String, required: true, unique:true},
     email:{type: String, required: true, unique: true},
     password:{type: String, required: true},
+    //https://www.slingacademy.com/article/how-to-use-enums-in-mongoose-a-practical-guide/
+    //https://thewebdev.info/2022/02/28/how-to-create-and-use-enums-in-mongoose/
+    // use this
     role: {type: String, enum: ['Admin', 'User'], default: 'User'}
-
 })
 
 
